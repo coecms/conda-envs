304707e36b2ed6f74d058830e9069aac3565d8bc
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2017-09-05 15:43:52 +1000

    Removed code to check top_level.txt. Not enough packages have defined imports. Made import fail by default but added option to specify level of import

--- test_environment.py
+++ test_environment.py
@@ -59,91 +59,55 @@ def package_list():
     """
     return _call_and_parse(['list', '--json'])
 
-def find_file(paths, filename):
-
-    filepath = None
-    for path in paths:
-
-        if not os.path.isdir(path): continue
-            
-        print(path)
-
-        try:
-            output = subprocess.check_output(['find',path,'-name',filename])
-            filepath = output.decode('ascii').rstrip("\n")
-        except Exception as e:
-            print("path: {} filename: {}\n".format(path,filename))
-            print(str(e))
-            continue
-
-    return filepath
-            
-def check_packages(exhaustive=True):
+def check_packages(dirs=None,depth=1):
     """
     For each installed package rummage through python cache directories looking for 
     modules to try and load, in order to check the integrity of the conda installation
+
+    Keyword arguments:
+    dirs  -- array of conda package cache directories to search 
+    depth -- integer determing the depth of module import namespace. Default 
+             only imports top level
     """
 
-    pkgcachedirs = package_dirs()
-
-    pkgcachedirs.insert(0, "/short/w35/saw562/conda/pkgs")
-        
-    print(pkgcachedirs)
+    if dirs is None:
+        pkgcachedirs = package_dirs()
+    else:
+        pkgcachedirs = dirs
 
     for package in package_list():
+        
+        # Look for the "files" file in the package cache directories. This, perhaps
+        # unsurprisingly, lists all the files in the package
+        files = None
+        for directory in pkgcachedirs:
+            tmp = os.path.join(directory, package["dist_name"], 'info','files')
+            if os.path.exists(tmp):
+                files = tmp
+                break
 
-        if not exhaustive:
-            
-            toplevel = find_file([ os.path.join(d, package["dist_name"]) for d in pkgcachedirs], 'top_level.txt')
-            
-            if toplevel is None:
-                print("cachedir not found for package: {}\n".format(package))
-            if len(toplevel) == 0:
-                print("no toplevel.txt file found in cachedir")
-            else:
-                with open(toplevel, 'r') as infile:
-                    for name in infile:
-                        name = name.rstrip("\n")
-                        try:
-                            import_module(name, package=None)
-                            # except ModuleNotFoundError :
-                            #     print("Module not found, ignoring {}".format(package['name']))
-                        except Exception as e:
-                            print(str(e))
-                        else:
-                            print("Imported {} successfully".format(name))
-                            # Skip to next package unless specify exhaustive testing
-                            # if not exhaustive: continue
-
+        if files is None:
+            print("no files file found for package: {}\n".format(package))
         else:
-
-            # Look for the "files" file which, perhaps unsurprisingly, lists all the
-            # files in the package
-            files = None
-            for directory in pkgcachedirs:
-                tmp = os.path.join(directory, package["dist_name"], 'info','files')
-                if os.path.exists(tmp):
-                    files = tmp
-                    break
-
-            if files is None:
-                print("no files file found for package: {}\n".format(package))
-            else:
-                with open(files, 'r') as infile:
-                    for name in infile:
-                        name = name.rstrip("\n")
-                        if name.endswith('__init__.py'):
-                            name = os.path.dirname(name)
-                            names = []
-                            for element in nextdir(name):
-                                if element == 'site-packages': break
-                                names.append(element)
+            # Read in all the file paths, find directories containing an __init__.py
+            # transform the directory path into a module name and attempt to import it
+            print(files)
+            with open(files, 'r') as infile:
+                for name in infile:
+                    name = name.rstrip("\n")
+                    if name.endswith('__init__.py'):
+                        name = os.path.dirname(name)
+                        names = []
+                        for element in nextdir(name):
+                            if element == 'site-packages': break
+                            names.append(element)
+                        if len(names) <= depth:
                             modname = ".".join(reversed(names))
                             if importmodule(modname):
                                 print("Imported {} successfully".format(modname))
-                            
 
-def importmodule(modname):
+
+def importmodule(modname,fail=True):
 
     imported_ok = False
     try:
@@ -151,6 +115,7 @@ def importmodule(modname):
     except Exception as e:
         warnstring = "Failed to import {} : Reported error: {}".format(modname,str(e))
         warnings.warn(UserWarning(warnstring))
+        if fail: raise
     else:
         imported_ok = True
 
