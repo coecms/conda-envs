db943621d7fa351de509c9e6b9b0b8b74f67ae24
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2018-11-13 17:11:07 +1100

    - Now use bespoke version of walk_packages
    - Added ability to skip packages, listed in 'skips' file
    
    - Moved preloaded modules to a file 'preloads'
    
    - Previous issue with psutil fixed with skips

--- test_environment.py
+++ test_environment.py
@@ -23,15 +23,75 @@ import subprocess
 
 exceptions = set()
 
-def import_exceptions():
+def my_walk_packages(path=None, prefix='', onerror=None, skips=None):
+    """Yields ModuleInfo for all modules recursively
+    on path, or, if path is None, all accessible modules.
+
+    'path' should be either None or a list of paths to look for
+    modules in.
+
+    'prefix' is a string to output on the front of every module name
+    on output.
+
+    Note that this function must import all *packages* (NOT all
+    modules!) on the given path, in order to access the __path__
+    attribute to find submodules.
+
+    'onerror' is a function which gets called with one argument (the
+    name of the package which was being imported) if any exception
+    occurs while trying to import a package.  If no onerror function is
+    supplied, ImportErrors are caught and ignored, while all other
+    exceptions are propagated, terminating the search.
+
+    Examples:
+
+    # list all modules python can access
+    walk_packages()
+
+    # list all submodules of ctypes
+    walk_packages(ctypes.__path__, ctypes.__name__+'.')
     """
-    Return a set of python modules that are exempt in check_packages
+
+    def seen(p, m={}):
+        if p in m:
+            return True
+        m[p] = True
+
+    for info in iter_modules(path, prefix):
+        yield info
+
+        if info.name not in skips:
+            if info.ispkg and info.name:
+                print("Importing {}".format(info.name))
+                try:
+                    __import__(info.name)
+                except ImportError:
+                    if onerror is not None:
+                        onerror(info.name)
+                except Exception:
+                    if onerror is not None:
+                        onerror(info.name)
+                    else:
+                        raise
+                else:
+                    path = getattr(sys.modules[info.name], '__path__', None) or []
+
+                    # don't traverse path items we've seen before
+                    path = [p for p in path if not seen(p)]
+
+                    yield from my_walk_packages(path, info.name+'.', onerror, skips)
+        else:
+            print("Skipping {}".format(info.name))
+
+
+def import_modules(filename):
     """
-    exceptionsfile = 'exceptions'
+    Return a set of python modules from a file
+    """
     mods = set()
 
-    if os.path.exists(exceptionsfile):
-        with open(exceptionsfile, 'r') as f:
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
             for line in f:
                 mods.add(line.rstrip("\n"))
 
@@ -53,16 +113,19 @@ def handle_error(name):
             raise
 
 def test_walk_packages():
-    import pysal
-    import tables
-    import skimage.data
-    import sklearn
-    import sklearn.covariance
-    import sklearn.manifold
     global exceptions
-    exceptions = import_exceptions()
-    for importer, name, ispkg in walk_packages(onerror=handle_error):
-        # print("Importing {}".format(name))
+    for mod in import_modules('preloads'):
+        try:
+            print("Preloading {}".format(mod))
+            __import__(mod)
+        except:
+            print("Error preloading")
+
+    exceptions = import_modules('exceptions')
+    skips = import_modules('skips')
+    print('Exceptions: {}'.format(exceptions))
+    print('Skips: {}'.format(skips))
+    for importer, name, ispkg in my_walk_packages(onerror=handle_error,skips=skips):
         pass
 
     if len(exceptions) > 0:
