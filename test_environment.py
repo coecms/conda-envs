ff753df9cf300a679b69ddc20f6367adc5e96823
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2018-11-14 10:04:44 +1100

    Coalesced all module control configutation into a single file, testconfig.yml
    This is a dict with possible keys of 'exception', 'preload' and 'skip'.
    
    The modules in preload are loaded before the walk_packages as there is some
    order of operation issue with the tests that isn't evident in every day use.
    
    The modules in skip are not loaded when packages are walked, as they cause
    errors that do gracefully exit with clean exceptions.
    
    The modules in exception are allowed to be loaded, fail, and raise an exception,
    but these are monitored and any untripped exception is reported. In this way
    they can be removed when no longer required.

--- test_environment.py
+++ test_environment.py
@@ -20,10 +20,11 @@ from pkgutil import walk_packages, iter_modules
 import warnings
 import pytest
 import subprocess
+import yaml
 
-exceptions = set()
+debug = False
 
-def my_walk_packages(path=None, prefix='', onerror=None, skips=None):
+def my_walk_packages(path=None, prefix='', onerror=None, skip=None):
     """Yields ModuleInfo for all modules recursively
     on path, or, if path is None, all accessible modules.
 
@@ -60,9 +61,9 @@ def my_walk_packages(path=None, prefix='', onerror=None, skips=None):
     for info in iter_modules(path, prefix):
         yield info
 
-        if info.name not in skips:
+        if info.name not in skip:
             if info.ispkg and info.name:
-                print("Importing {}".format(info.name))
+                if debug: print("Importing {}".format(info.name))
                 try:
                     __import__(info.name)
                 except ImportError:
@@ -79,57 +80,62 @@ def my_walk_packages(path=None, prefix='', onerror=None, skips=None):
                     # don't traverse path items we've seen before
                     path = [p for p in path if not seen(p)]
 
-                    yield from my_walk_packages(path, info.name+'.', onerror, skips)
+                    yield from my_walk_packages(path, info.name+'.', onerror, skip)
         else:
             print("Skipping {}".format(info.name))
 
 
-def import_modules(filename):
+def import_config(filename):
     """
-    Return a set of python modules from a file
+    Return a configuration dictionary
     """
-    mods = set()
+    try:
+        with open(filename, 'r') as config_file:
+            config = yaml.load(config_file)
+    except IOError as exc:
+        if exc.errno == errno.ENOENT:
+            print('Warning: config file {0} not found!'
+                  .format(config_fname))
+            config = {}
+        else:
+            raise
 
-    if os.path.exists(filename):
-        with open(filename, 'r') as f:
-            for line in f:
-                mods.add(line.rstrip("\n"))
-
-    return mods
+    return config
 
 def handle_error(name):
-    global exceptions
+    global exception
     try:
         raise
     except Warning as e:
-        print("Warning loading %s"%name, file=sys.stderr)
+        print("Warning loading {}".format(name), file=sys.stderr)
     except Exception as e:
-        print("Error loading %s"%name, file=sys.stderr)
-        if name in exceptions:
-            print("{} listed in exceptions, ignoring import error".format(name))
-            exceptions.remove(name)
+        print("Error loading {}".format(name), file=sys.stderr)
+        if name in exception:
+            print("{} listed as exception, ignoring import error".format(name))
+            exception.remove(name)
             pass
         else:
             raise
 
 def test_walk_packages():
-    global exceptions
-    for mod in import_modules('preloads'):
+    global exception
+    config = import_config('testconfig.yml')
+    for mod in config.get('preload',[]):
         try:
-            print("Preloading {}".format(mod))
+            if debug: print("Preloading {}".format(mod))
             __import__(mod)
         except:
-            print("Error preloading")
+            print("Error preloading {}".format(mod))
 
-    exceptions = import_modules('exceptions')
-    skips = import_modules('skips')
-    print('Exceptions: {}'.format(exceptions))
-    print('Skips: {}'.format(skips))
-    for importer, name, ispkg in my_walk_packages(onerror=handle_error,skips=skips):
+    exception = set(config.get('exception',None))
+    skip = set(config.get('skip',None))
+    if debug: print('Exception: {}'.format(exception))
+    if debug: print('Skip: {}'.format(skip))
+    for importer, name, ispkg in my_walk_packages(onerror=handle_error,skip=skip):
         pass
 
-    if len(exceptions) > 0:
-        warnstring = "Untripped exceptions should be removed: {}".format(exceptions)
+    if len(exception) > 0:
+        warnstring = "Untripped exceptions should be removed: {}".format(exception)
         warnings.warn(UserWarning(warnstring))
 
 def test_import():
@@ -138,10 +144,10 @@ def test_import():
             try:
                 __import__(info.name)
             except Warning:
-                print("Warning loading %s"%info.name, file=sys.stderr)
+                print("Warning loading {}".format(info.name), file=sys.stderr)
                 pass
             except:
-                print("Error loading %s"%info.name, file=sys.stderr)
+                print("Error loading {}".format(info.name), file=sys.stderr)
                 raise
 
 def test_cdo():
