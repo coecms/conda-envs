fdd4f6b713edda5120889ec438cd322f0b815fc7
Scott Wales <scott.wales@unimelb.edu.au>
2021-10-11 10:25:38 +1100

    Turn off debug in testing

--- test_environment.py
+++ test_environment.py
@@ -22,7 +22,7 @@ import pytest
 import subprocess
 import yaml
 
-debug = True
+debug = False
 
 def my_walk_packages(path=None, prefix='', onerror=None, skip=None):
     """Yields ModuleInfo for all modules recursively
